343b62a466f877ec1648f3d5e2fa8311de468021
ray0305 <ray0305@gmail.com>
2025-12-24 06:51:30 +0800

    feat: 新增說明文件

--- src/pages/VueDemo/ReactiveData/Computed/index.vue
+++ src/pages/VueDemo/ReactiveData/Computed/index.vue
@@ -1,3 +1,19 @@
+<script setup lang="ts">
+import { computed, reactive, ref } from 'vue';
+// 建立 reactive
+const user = reactive({
+  name: '王大明',
+  age: 10
+})
+// 建立 computed 屬性
+const isAdult = computed(() => {
+  const adultDesc = user.age >= 18 ? "已成年" : "未成年"
+  return `我是${user.name}，今年${user.age}歲，${adultDesc}。`
+})
+// 使用 computed 屬性
+const message = ref(isAdult.value)
+</script>
+
 <template>
-    Computed Demo
+  <h1>{{ message }}</h1>
 </template>
--- src/pages/VueDemo/ReactiveData/Reactive/index.vue
+++ src/pages/VueDemo/ReactiveData/Reactive/index.vue
@@ -1,3 +1,20 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+// 建立 reactive
+const state = reactive({
+    count: 0
+})
+// 修改 reactive 的值
+const add = () => {
+    state.count = state.count + 1
+}   
+</script>
+
+
 <template>
-    Reactive Demo
+    <h1>reactive - 響應式對象</h1>
+    <div>
+        <p>目前數值：{{ state.count }}</p>
+        <button @click="add">加 1</button>
+    </div>
 </template>>
